faf4a852bb2dc699a2822ef6f8866cb857204ffe
TomiAltero <tomasaltero@gmail.com>
2024-05-20 20:45:18 -0300

    correccion seguridad configuracion

--- backend/backend-downisup/src/models/index.js
+++ backend/backend-downisup/src/models/index.js
@@ -1,19 +1,30 @@
 'use strict';
 
+require('dotenv').config(); // Cargar variables de entorno desde el archivo .env
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.USE_ENV_VARIABLE) {
+  sequelize = new Sequelize(process.env[process.env.USE_ENV_VARIABLE], {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  });
 }
 
 fs
@@ -41,3 +52,4 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
