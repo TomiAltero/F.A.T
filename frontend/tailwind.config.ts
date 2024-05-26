216292380c1d0fdb9870ce2a687cca4bfef9e3b5
FacundoBisio <facubisio433@gmail.com>
2024-05-26 20:06:52 -0300

    Front-end/Sign-up

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -19,7 +19,9 @@ const config: Config = {
 
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),
+            require('@tailwindcss/forms'),],
+          
 }
 };
 
