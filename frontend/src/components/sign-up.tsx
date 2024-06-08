3603334f27fb9edb8a11393df311fc461d23f6a6
TomiAltero <tomasaltero@gmail.com>
2024-06-08 17:50:52 -0300

    sign up

--- frontend/src/components/sign-up.tsx
+++ frontend/src/components/sign-up.tsx
@@ -1,33 +1,39 @@
-'use client'
-import { CardTitle, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { nunito } from "@/components/ui/fonts"
-import Image from "next/image"
-import React, { useState } from "react"
-import axios from "axios"
+"use client";
+import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React, { useState } from "react";
+import axios from "axios";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
+import "react-toastify/dist/ReactToastify.css";
 
 export function SignUp() {
-  const [showPassword, setShowPassword] = useState<boolean>(false)
-  const [email, setEmail] = useState<string>('');
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [nombre, setNombre] = useState<string>('');
-  const [apellido, setApellido] = useState<string>('');
-  const [isMatch, setIsMatch] = useState<boolean>(true)
-  const [errors, setErrors] = useState<{ msg: string }[]>([])
-  const [successfulMessage, setSuccessfulMessage] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [nombre, setNombre] = useState<string>("");
+  const [apellido, setApellido] = useState<string>("");
+  const [isMatch, setIsMatch] = useState<boolean>(true);
+  const [errors, setErrors] = useState<{ msg: string }[]>([]);
+  const [successfulMessage, setSuccessfulMessage] = useState<string | null>(
+    null,
+  );
 
-  const handleCheckBoxChange = () => setShowPassword(!showPassword)
+  const handleCheckBoxChange = () => setShowPassword(!showPassword);
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
     setIsMatch(event.target.value === confirmPassword);
   };
 
-  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     setConfirmPassword(event.target.value);
     setIsMatch(event.target.value === password);
   };
@@ -36,24 +42,66 @@ export function SignUp() {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/api/usuarios', {
+      const response = await axios.post("http://localhost:5000/api/usuarios", {
         username,
         email,
         password,
         nombre,
-        apellido
+        apellido,
       });
 
-      console.log('User registration successful:', response.data);
-      setErrors([]);
-      setSuccessfulMessage('Usuario registrado exitosamente');
+      Toastify({
+        text: "Usuario registrado exitosamente",
+        duration: 6000,
+        position: "right",
+        style: {
+          background: "#009933",
+          color: "#FFFFFF",
+          fontSize: "14px",
+          padding: "10px",
+          borderRadius: "4px",
+          fontWeight: "bold",
+          marginTop: "70px",
+        },
+      }).showToast();
 
+      console.log("User registration successful:", response.data);
+      setErrors([]);
+      setSuccessfulMessage("Usuario registrado exitosamente");
+
+      setNombre("");
+      setApellido("");
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
     } catch (error: any) {
-      console.error('Error registering user:', error.response?.data || error.message);
+      console.error(
+        "Error registrando el usuario:",
+        error.response?.data || error.message,
+      );
       if (error.response && error.response.data && error.response.data.errors) {
-        setErrors(error.response.data.errors);
+        const validationErrors = error.response.data.errors.map(
+          (error: any) => error.msg,
+        );
+        validationErrors.forEach((errorText: string) => {
+          Toastify({
+            text: errorText,
+            duration: 20000,
+            position: "right",
+            style: {
+              background: "#FF0000",
+              color: "#FFFFFF",
+              fontSize: "14px",
+              padding: "10px",
+              borderRadius: "4px",
+              fontWeight: "bold",
+              marginTop: "70px",
+            },
+          }).showToast();
+        });
       } else {
-        setErrors([{ msg: 'Hubo un error al registrar el usuario' }]);
+        setErrors([{ msg: "Hubo un error al registrar el usuario" }]);
       }
       setSuccessfulMessage(null);
     }
@@ -69,21 +117,26 @@ export function SignUp() {
           {errors.length > 0 && (
             <section className="my-3">
               {errors.map((error, index) => (
-                <p key={index} className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-red-600 p-3 my-2">{error.msg}</p>
+                <p
+                  key={index}
+                  className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-red-600 p-3 my-2"
+                >
+                  {error.msg}
+                </p>
               ))}
-            </section>
-          )}
-          {successfulMessage && (
-            <section className="my-3">
-              <p className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-green-600 p-3 my-2">{successfulMessage}</p>
             </section>
           )}
           <section className="my-3 -mt-5">
             <section className="flex flex-row space-x-4">
-              <article className="my-2">
-                <Label className="block text-[14px] leading-6 text-custom-blue" htmlFor="nombre">Nombre</Label>
+              <article className="space-y-2 my-2">
+                <Label
+                  className="block text-xs font-bold leading-6 text-blue-900"
+                  htmlFor="nombre"
+                >
+                  Nombre
+                </Label>
                 <Input
-                  className="border"
+                  className="rounded-xl border"
                   id="nombre"
                   name="nombre"
                   placeholder="Ingrese su nombre"
@@ -93,10 +146,15 @@ export function SignUp() {
                   onChange={(e) => setNombre(e.target.value)}
                 />
               </article>
-              <article className="my-2">
-                <Label className="block text-[14px] leading-6 text-custom-blue" htmlFor="apellido">Apellido</Label>
+              <article className="space-y-2 my-2">
+                <Label
+                  className="block text-xs font-bold leading-6 text-blue-900"
+                  htmlFor="apellido"
+                >
+                  Apellido
+                </Label>
                 <Input
-                  className="border"
+                  className="rounded-xl border"
                   id="apellido"
                   name="apellido"
                   placeholder="Ingrese su apellido"
@@ -108,24 +166,34 @@ export function SignUp() {
               </article>
             </section>
 
-            <article className="my-2">
-              <Label className="block text-[14px] leading-6 text-custom-blue" htmlFor="email">Nombre de Usuario</Label>
+            <article className="space-y-2 my-2">
+              <Label
+                className="block text-xs font-bold leading-6 text-blue-900"
+                htmlFor="username"
+              >
+                Nombre de Usuario
+              </Label>
               <Input
-                className="border"
+                className="rounded-xl border"
                 id="username"
                 name="username"
                 placeholder="Ingrese su nombre de usuario"
                 required
-                type="username"
+                type="text"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
             </article>
 
-            <article className="my-2">
-              <Label className="block text-[14px] leading-6 text-custom-blue" htmlFor="email">Email</Label>
+            <article className="space-y-2 my-2">
+              <Label
+                className="block text-xs font-bold leading-6 text-blue-900"
+                htmlFor="email"
+              >
+                Email
+              </Label>
               <Input
-                className="border"
+                className="rounded-xl border"
                 id="email"
                 name="email"
                 placeholder="Ingrese su correo electrónico"
@@ -135,32 +203,45 @@ export function SignUp() {
                 onChange={(e) => setEmail(e.target.value)}
               />
             </article>
-            <article className="my-2">
-              <Label className="block text-[14px] leading-6 text-custom-blue" htmlFor="password">Contraseña</Label>
+            <article className="space-y-2 my-2">
+              <Label
+                className="block text-xs font-bold leading-6 text-blue-900"
+                htmlFor="password"
+              >
+                Contraseña
+              </Label>
               <article className="space-y-3">
                 <Input
                   id="password"
                   name="password"
-                  className="border"
+                  className="rounded-xl border"
                   placeholder="Ingrese su contraseña"
                   required
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                   value={password}
                   onChange={handlePasswordChange}
                 />
-                <Label className="block text-[14px] leading-6 text-custom-blue" htmlFor="password2">Confirmar Contraseña</Label>
+                <Label
+                  className="block text-xs font-bold leading-6 text-blue-900"
+                  htmlFor="password2"
+                >
+                  Confirmar Contraseña
+                </Label>
                 <Input
                   id="password2"
-                  className="border"
+                  className="rounded-xl border"
                   placeholder="Repita la contraseña"
                   required
-                  type={showPassword ? 'text' : 'password'}
+                  type={showPassword ? "text" : "password"}
                   value={confirmPassword}
                   onChange={handleConfirmPasswordChange}
                 />
                 <article>
-                  {!isMatch ? <p className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-red-600 p-2 my-2">Las contraseñas no coinciden</p>
-                    : <p></p>}
+                  {!isMatch && (
+                    <p className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-red-600 p-2 my-2">
+                      Las contraseñas no coinciden
+                    </p>
+                  )}
                 </article>
               </article>
               <article className="flex gap-x-2 items-center">
@@ -170,15 +251,20 @@ export function SignUp() {
                   onChange={handleCheckBoxChange}
                   id="show"
                 />
-                <Label className="block text-[10px] leading-6 text-grey-900" htmlFor="show">Mostrar Contraseña</Label>
+                <Label
+                  className="block text-xs font-medium leading-6 text-gray-900"
+                  htmlFor="show"
+                >
+                  Mostrar contraseña
+                </Label>
               </article>
             </article>
             <article className="flex justify-center w-full mt-5">
               <Button
-                variant={isMatch ? 'secondary' : 'ghost'}
                 disabled={!isMatch}
-                type='submit'
-                className="w-full rounded-2xl px-3 py-2 text-sm font-semi bold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                className="w-full rounded-2xl bg-custom-blue px-3 py-2 text-sm font-semi bold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
+                type="submit"
+              >
                 Continuar
               </Button>
             </article>
@@ -188,4 +274,3 @@ export function SignUp() {
     </form>
   );
 }
-
