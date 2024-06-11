6165e565e63046996b166c335ed0f584e9addd48
TomiAltero <tomasaltero@gmail.com>
2024-06-11 11:03:43 -0300

    correccion

--- frontend/src/components/ui/FormSignUp.tsx
+++ frontend/src/components/ui/FormSignUp.tsx
@@ -1,28 +1,35 @@
-'use client'
-import { CardTitle, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import React, { useState } from "react"
-import axios from "axios"
-import InputAdornments from "./ui/inputAdornments";
-import TextField from '@mui/material/TextField';
-import 'react-toastify/dist/ReactToastify.css';
+"use client";
+import {
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import axios from "axios";
+import InputAdornments from "../ui/inputAdornments";
+import TextField from "@mui/material/TextField";
+import "react-toastify/dist/ReactToastify.css";
 import "toastify-js/src/toastify.css";
 import Toastify from "toastify-js";
-
+import React, { useState } from "react";
 
 export function FormSignUp() {
-  const [showPassword, setShowPassword] = useState<boolean>(false)
-  const [email, setEmail] = useState<string>('');
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [nombre, setNombre] = useState<string>('');
-  const [apellido, setApellido] = useState<string>('');
-  const [isMatch, setIsMatch] = useState<boolean>(true)
-  const [errors, setErrors] = useState<{ msg: string }[]>([])
-  const [successfulMessage, setSuccessfulMessage] = useState<string | null>(null);
-  const [registrationSuccess, setRegistrationSuccess] = useState<boolean>(false);
-
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [nombre, setNombre] = useState<string>("");
+  const [apellido, setApellido] = useState<string>("");
+  const [isMatch, setIsMatch] = useState<boolean>(true);
+  const [errors, setErrors] = useState<{ msg: string }[]>([]);
+  const [successfulMessage, setSuccessfulMessage] = useState<string | null>(
+    null,
+  );
+  const [registrationSuccess, setRegistrationSuccess] =
+    useState<boolean>(false);
 
   const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newPassword = event.target.value;
@@ -30,7 +37,9 @@ export function FormSignUp() {
     setIsMatch(newPassword === confirmPassword);
   };
 
-  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleConfirmPasswordChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
     const newConfirmPassword = event.target.value;
     setConfirmPassword(newConfirmPassword);
     setIsMatch(password === newConfirmPassword);
@@ -40,26 +49,25 @@ export function FormSignUp() {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/api/usuarios', {
+      const response = await axios.post("http://localhost:5000/api/usuarios", {
         username,
         email,
         password,
         nombre,
-        apellido
+        apellido,
       });
 
-      console.log('User registration successful:', response.data);
+      console.log("User registration successful:", response.data);
       setErrors([]);
-      setSuccessfulMessage('Usuario registrado exitosamente');
+      setSuccessfulMessage("Usuario registrado exitosamente");
 
-      setUsername('');
-      setEmail('');
-      setPassword('');
-      setConfirmPassword('');
-      setNombre('');
-      setApellido('');
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setNombre("");
+      setApellido("");
       setIsMatch(true);
-
 
       Toastify({
         text: "Usuario registrado exitosamente",
@@ -76,16 +84,18 @@ export function FormSignUp() {
         },
       }).showToast();
     } catch (error: any) {
-      console.error('Error registrando el usuario:', error);
+      console.error("Error registrando el usuario:", error);
       setErrors([{ msg: error.response.data.message }]);
-      toast.error('Error registrando el usuario');
+      toast.error("Error registrando el usuario");
     }
   };
   return (
     <form onSubmit={handleFormSubmit}>
       <Card className="max-w-md mx-auto mt-8">
         <CardHeader>
-          <CardTitle className="text-center text-custom-blue mt-5">Registro</CardTitle>
+          <CardTitle className="text-center text-custom-blue mt-5">
+            Registro
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <section className="space-y-3">
@@ -103,8 +113,9 @@ export function FormSignUp() {
                 InputLabelProps={{ required: false }}
               />
             </article>
-            <article >
-              <TextField className="-mt-1.5"
+            <article>
+              <TextField
+                className="-mt-1.5"
                 id="email"
                 label="Email"
                 type="email"
@@ -118,7 +129,8 @@ export function FormSignUp() {
               />
             </article>
             <article>
-              <TextField className="-mt-1.5"
+              <TextField
+                className="-mt-1.5"
                 id="nombre"
                 label="Nombre"
                 type="text"
@@ -132,7 +144,8 @@ export function FormSignUp() {
               />
             </article>
             <article>
-              <TextField className="-mt-1.5"
+              <TextField
+                className="-mt-1.5"
                 id="apellido"
                 label="Apellido"
                 type="text"
@@ -150,15 +163,20 @@ export function FormSignUp() {
 
             <article>
               <article>
-                {!isMatch && <p className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-red-600 p-2 my-2">Las contraseñas no coinciden</p>}
+                {!isMatch && (
+                  <p className="transition-all rounded-2xl border ease-in-out delay-200 text-sm text-center font-bold bg-red-600 p-2 my-2">
+                    Las contraseñas no coinciden
+                  </p>
+                )}
               </article>
             </article>
             <article className="flex justify-center w-full mt-5 ">
               <Button
-                variant={isMatch ? 'secondary' : 'ghost'}
+                variant={isMatch ? "secondary" : "ghost"}
                 disabled={!isMatch}
-                type='submit'
-                className="w-full rounded-2xl px-3 py-2 text-sm font-semi bold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                type="submit"
+                className="w-full rounded-2xl px-3 py-2 text-sm font-semi bold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
                 Continuar
               </Button>
             </article>
@@ -168,4 +186,3 @@ export function FormSignUp() {
     </form>
   );
 }
-
